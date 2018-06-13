e07032cb1274fbf02fbd61d8935026a08c99e2f4
yudai <yu_dai1996@aliyun.com>
2018-06-13 13:56:44 +0800

    Update imformationCoefficient_calculation.py

--- imformationCoefficient_calculation.py
+++ imformationCoefficient_calculation.py
@@ -34,25 +34,27 @@ def get_quarterly_data(financial_indicator,stocks,quarter):
             else:
                 return data.diff().dropna().values.tolist()
 
-    if str(financial_indicator).startswith("fundamentals.balance_sheet"):
-        return df.major_xs(quarter)
+    original_factor_values = df.major_xs(quarter)
+    announce_dates = pd.to_datetime(original_factor_values['announce_date'],format='%Y%m%d')
+
+    if str(financial_indicator).startswith("StkBalaGen.total_assets"):
+        original_factor_values.columns = ['factor_values','announce_date']
+        original_factor_values['announce_date'] = announce_dates
+        return original_factor_values
     else:
         data = df.iloc[0]
         factor_values = pd.DataFrame(pd.DataFrame(data.apply(lambda x:_get_indicator_change(x)).to_dict()).pct_change().iloc[-1]).astype(float)
-        factor_values['announce_date'] = pd.to_datetime(df.major_xs(quarter)['announce_date'],format="%Y%m%d")
+        factor_values.columns = ['factor_values']
+        factor_values['announce_date'] = announce_dates
         return factor_values
 
 
-def calc_quarterly_imformationCoefficient(financial_indicator,stocks,quarter,N=22):
+def calc_quarterly_imformationCoefficient(factor_values,N=22):
     """
-    :param financial_indicator:
-    :param stocks:股票列表
-    :param quarter:财报的年份和季度，例"2107q3"
+    :param factor_values: 因子值
     :param N:滞后的交易日数目
     :return:返回该季度时期的相关系数(spearman)
     """
-    df = get_quarterly_data(financial_indicator, stocks, quarter).dropna()
-
     def _calc_single_returns(stock,date):
         try:
             _price = get_price(stock,date,date+np.timedelta64(40,"D"),fields='close').iloc[-N:]
@@ -60,28 +62,41 @@ def calc_quarterly_imformationCoefficient(financial_indicator,stocks,quarter,N=2
             return _returns
         except:
             return np.nan
-    returns = pd.Series([_calc_single_returns(i[0],i[1].iloc[1]) for i in df.iterrows()],index=df.index)
-    ic = st.spearmanr(df.iloc[:,0],returns,nan_policy='omit')[0]
+    returns = pd.Series([_calc_single_returns(i[0],i[1]['announce_date']) for i in factor_values.iterrows()],index=factor_values.index)
+    ic = st.spearmanr(factor_values['factor_values'],returns,nan_policy='omit')[0]
     return ic
     
 #--------------------------------------------The following for testing-------------------------------------------
 
-financial_indicator = fundamentals.income_statement.net_profit
-# stocks
-# all_a_stocks = all_instruments(type="CS").order_book_id.tolist()
+def calc_periods_imformationCoefficient(financial_indicator,start_year,end_year,stocksPool):
 
-mapping_dates = {"q1":"03-31","q2":"06-30",'q3':"09-30",'q4':"12-31"}
-all_quarters = [str(i)+str(j) for j in mapping_dates.keys() for i in np.arange(2014,2018,1)]
-all_end_dates = [str(i)+"-"+ j for j in list(mapping_dates.values()) for i in np.arange(2014,2018,1)]
-all_end_dates.sort()
-all_quarters.sort()
-ics = {}
-for i,q in enumerate(all_quarters):
-    print(i)
-    ic = calc_quarterly_imformationCoefficient(financial_indicator,index_components("000300.XSHG",all_end_dates[i]),q,N=22)
-    ics[i] = ic
+    mapping_dates = {"q1":"03-31","q2":"06-30",'q3':"09-30",'q4':"12-31"}
+    all_quarters = sorted([str(i)+str(j) for j in mapping_dates.keys() for i in np.arange(start_year,end_year+1,1)])
+    all_end_dates = sorted([str(i)+"-"+ j for j in list(mapping_dates.values()) for i in np.arange(start_year,end_year+1,1)])
+    ics = {}
+    for i,q in enumerate(all_quarters):
+        print("calculating the quarter === %s"%(q))
+        try:
+            _factor_values = get_quarterly_data(financial_indicator,index_components(stocksPool,all_end_dates[i]),q)
+            ic = calc_quarterly_imformationCoefficient(_factor_values,N=22)
+        except:
+            ic = np.nan
+        ics[q] = ic
+    return pd.Series(ics)
 
-import matplotlib.pyplot as plt
-fig,ax = plt.subplots()
-pd.Series(ics).plot(kind='bar',ax=ax)
-ax.set_xticklabels(all_quarters)
+
+financial_indicator_roe = fundamentals.financial_indicator.du_return_on_equity
+financial_indicator_NPM = fundamentals.financial_indicator.du_profit_margin
+financial_indicator_EM = fundamentals.financial_indicator.du_equity_multiplier
+financial_indicator_AU = fundamentals.financial_indicator.du_asset_turnover_ratio
+
+ics_roe_CSI500 = calc_periods_imformationCoefficient(financial_indicator_roe,2014,2018,"000905.XSHG")
+ics_NPM_CSI500 = calc_periods_imformationCoefficient(financial_indicator_NPM,2014,2018,"000905.XSHG")
+ics_AU_CSI500 = calc_periods_imformationCoefficient(financial_indicator_AU,2014,2018,"000905.XSHG")
+ics_EM_CSI500 = calc_periods_imformationCoefficient(financial_indicator_EM,2014,2018,"000905.XSHG")
+
+ics_roe_CSI300 = calc_periods_imformationCoefficient(financial_indicator_roe,2014,2018,"000300.XSHG")
+ics_NPM_CSI300 = calc_periods_imformationCoefficient(financial_indicator_NPM,2014,2018,"000300.XSHG")
+ics_AU_CSI300 = calc_periods_imformationCoefficient(financial_indicator_AU,2014,2018,"000300.XSHG")
+ics_EM_CSI300 = calc_periods_imformationCoefficient(financial_indicator_EM,2014,2018,"000300.XSHG")
+
