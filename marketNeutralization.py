3068593c57059606e279453593de15f2ee963fa6
yudai <yu_dai1996@aliyun.com>
2018-06-29 14:21:12 +0800

    Update marketNeutralization.py

--- marketNeutralization.py
+++ marketNeutralization.py
@@ -7,9 +7,10 @@ import rqdatac
 rqdatac.init('rice','rice',('192.168.10.64',16008))
 from functools import reduce
 
-# date="2017-06-01"
 
-def get_subnew_stocks(stocks,date,subnewThres):
+
+
+def subnew_stocks_filter(stocks,date,subnewThres=5):
     """
     # 获得某日上市小于N天的次新股
     :param stocks: list 股票列表
@@ -17,9 +18,10 @@ def get_subnew_stocks(stocks,date,subnewThres):
     :param N: int 次新股过滤的阈值
     :return: list 列表中的次新股
     """
-    return [s for s in stocks if (pd.Timestamp(date) - pd.Timestamp(rqdatac.instruments(s).listed_date)).days<=subnewThres]
+    # return [(rqdatac.instruments(s).listed_date) for s in stocks]
+    return [s for s in stocks if (pd.Timestamp(date) - pd.Timestamp(rqdatac.instruments(s).listed_date)).days>subnewThres]
 
-def get_st_stocks(stocks,date):
+def st_stocks_filter(stocks,date):
     """
     获得某日的ST类股票
     :param stocks: list 股票列表
@@ -27,17 +29,17 @@ def get_st_stocks(stocks,date):
     :return: list 列表中st的股票
     """
     st_series = rqdatac.is_st_stock(stocks,start_date=date,end_date=date).iloc[-1]
-    return st_series[st_series].index.tolist()
+    return st_series[~st_series].index.tolist()
 
-def filter_suspended_stocks(stocks,date):
+def suspended_stocks_filter(stocks,date):
     """
     :param stocks:股票列表
     :param date: 检验停牌日期
-    :return:
+    :return: list
     """
-    volume = rqdatac.get_price(stocks,start_date=date,end_date=date,fields="volume").iloc[0]
-    return volume[volume>0].index.tolist()
-    # return [s for s in stocks if rqdatac.is_suspended(s,start_date=date,end_date=date).values[0,0]]
+    # volume = rqdatac.get_price(stocks,start_date=date,end_date=date,fields="volume").iloc[0]
+    # return volume[volume>0].index.tolist()
+    return [s for s in stocks if not rqdatac.is_suspended(s,start_date=date,end_date=date).values[0,0]]
 
 def low_liquidity_filter(stocks,date,percentileThres):
     """
@@ -70,10 +72,9 @@ def noisy_stocks_filter(stocks,date,subnewThres=350,percentileThres=5):
     :param thres: 进行流动性和规模过滤的百分数阈值
     :return: list
     """
-    st_stocks = get_st_stocks(stocks,date)
-    subnew_stocks = get_subnew_stocks(stocks,date,subnewThres)
-    filtered_stocks = list(set(stocks)-set(st_stocks)-set(subnew_stocks))
-    filtered_stocks = filter_suspended_stocks(filtered_stocks, date)
+    filtered_stocks = st_stocks_filter(stocks,date)
+    filtered_stocks = subnew_stocks_filter(filtered_stocks,date,subnewThres)
+    filtered_stocks = suspended_stocks_filter(filtered_stocks, date)
     filtered_stocks = low_liquidity_filter(filtered_stocks,date,percentileThres)
     filtered_stocks = small_size_filter(filtered_stocks,date,percentileThres)
     return filtered_stocks
@@ -117,3 +118,37 @@ def get_explict_factor_returns(date):
         return priceChange[long_stockList].mean() - priceChange[short_stocksList].mean()
 
     return factor_exposures.apply(lambda x:_calc_single_explict_returns(x.name))
+
+
+def calc_index_explict_factor_returns(indexCode,date):
+
+    previous_trading_date = rqdatac.get_previous_trading_date(date)
+
+    indexComponents = rqdatac.index_components(indexCode,date=date)
+
+    factor_exposures = rqdatac.get_style_factor_exposure(indexComponents, previous_trading_date, previous_trading_date, "all").sort_index()
+    factor_exposures.index=factor_exposures.index.droplevel(1)
+    sizeBeta = factor_exposures[['size','beta']]
+
+    quantileGroup = sizeBeta.apply(lambda x:pd.cut(x,bins=3,labels=False)+1).reset_index()
+    quantileStocks = quantileGroup.groupby(['size','beta']).apply(lambda x:x.index.tolist())
+    market_neutralize_stocks = quantileStocks.apply(
+        lambda x: pd.Series(indexComponents).loc[x].values.tolist()).values.tolist()
+
+    def _calc_single_explict_returns(factor):
+
+        _factor_exposure = factor_exposures[factor]
+        def _deuce(series):
+            median = series.median()
+            return [series[series<=median].index.tolist(),series[series>median].index.tolist()]
+
+        deuceResults = np.array([_deuce(_factor_exposure[neutralized_stks]) for neutralized_stks in market_neutralize_stocks]).flatten()
+
+        short_stocksList = list(reduce(lambda x,y:set(x)|set(y),np.array([s for i,s in enumerate(deuceResults) if i%2==0])))
+        long_stockList = list(reduce(lambda x,y:set(x)|set(y),np.array([s for i,s in enumerate(deuceResults) if i%2==1])))
+
+        closePrice = rqdatac.get_price_change_rate(indexComponents,date,date).iloc[0]
+        return closePrice[long_stockList].mean() - closePrice[short_stocksList].mean()
+
+    return factor_exposures.apply(lambda x:_calc_single_explict_returns(x.name))
+
