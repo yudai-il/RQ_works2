3a2fbbc551f1126bc9e5b3fbb1797f0a1a7a140f
yudai <yu_dai1996@aliyun.com>
2018-07-27 18:24:20 +0800

    Update optimizer_toolkit.py

--- optimize/optimizer_toolkit.py
+++ optimize/optimizer_toolkit.py
@@ -7,6 +7,7 @@ import rqdatac
 import warnings
 from scipy.stats import norm
 import scipy.spatial as scsp
+from.exception import *
 
 def get_subnew_assets(order_book_ids,date,N,type="CS"):
     """
@@ -20,7 +21,6 @@ def get_subnew_assets(order_book_ids,date,N,type="CS"):
         return [s for s in order_book_ids if len(get_trading_dates(instruments(s).listed_date,date))<=N]
     elif type == "Fund":
         return [s for s in order_book_ids if len(get_trading_dates(fund.instruments(s).listed_date, date)) <= N]
-
 
 def get_st_stocks(stocks,date):
     """
@@ -106,8 +106,7 @@ def trackingError(x,**kwargs):
     """
     union_stocks, covMat,_index_weights = kwargs.get("union_stocks"),kwargs.get("covMat"),kwargs.get("index_weights")
     # vector of deviations
-    X = x - _index_weights.reindex(union_stocks).replace(np.nan, 0).values
-
+    X = x - _index_weights
     result = np.sqrt(np.dot(np.dot(X, covMat * 252),X))
     return result
 
@@ -119,7 +118,52 @@ def volatility(x,**kwargs):
     :param covMat:
     :return:
     """
-    return np.sqrt(np.dot(np.dot(x,covMat*252),x))
+    return np.dot(np.dot(x,covMat*252),x)
+
+def mean_variance(x,**kwargs):
+
+    annualized_return = kwargs.get("annualized_return")
+    risk_aversion_coefficient = kwargs.get("risk_aversion_coefficient")
+
+    if not isinstance(annualized_return,pd.Series):
+        raise Exception("在均值方差优化中请指定 预期收益")
+    portfolio_volatility = volatility(x,**kwargs)
+    return -x.dot(annualized_return) + np.multiply(risk_aversion_coefficient/2,portfolio_volatility)
+
+def maximizing_return(x,**kwargs):
+    annualized_return = kwargs.get("annualized_return")
+    return -x.dot(annualized_return)
+
+
+def maximizing_indicator(x,**kwargs):
+    indicator_series = kwargs.get("indicator_series")
+    return -x.dot(indicator_series)
+
+def risk_budgeting(x,**kwargs):
+    riskMetrics = kwargs.get("riskMetrics")
+    if riskMetrics == "volatility":
+        return np.sqrt(volatility(x, **kwargs))
+    else:
+        assert riskMetrics == "tracking_error"
+        return trackingError(x,**kwargs)
+
+def risk_parity(x,**kwargs):
+
+    c_m = kwargs.get("covMat")
+
+    def risk_parity_with_con_obj_fun(x):
+        temp1 = np.multiply(x, np.dot(c_m, x))
+        temp2 = temp1[:, None]
+        return np.sum(scsp.distance.pdist(temp2, "euclidean"))
+
+    if kwargs.get("with_cons"):
+        return risk_parity_with_con_obj_fun(x)
+    else:
+        c=15
+        res =  np.dot(x, np.dot(c_m, x)) - c * sum(np.log(x))
+        # print(res)
+        return res
+
 
 def industry_customized_constraint(order_book_ids,industryConstraints,date):
     """
@@ -141,15 +185,12 @@ def industry_customized_constraint(order_book_ids,industryConstraints,date):
         constrainted_industry = sorted(set(industries_labels)& set(industryConstraints.keys()))
 
     for industry in constrainted_industry:
-
         industry_stock_position = industries_labels.index.get_indexer(industries_labels[industries_labels==industry].index)
-
         lower,upper = industryConstraints.get(industry)[0],industryConstraints.get(industry)[1]
         constraints.append({"type":"ineq","fun":lambda x:sum(x[i] for i in industry_stock_position)-lower})
         constraints.append({"type":"ineq","fun":lambda x:sum(x[i] for i in -industry_stock_position)+upper})
 
     return constraints
-
 
 def industry_neutralize_constraint(order_book_ids, date,deviation,industryNeutral, benchmark):
     """
@@ -198,6 +239,7 @@ def industry_neutralize_constraint(order_book_ids, date,deviation,industryNeutra
     return constraints
 
 def validateConstraints(order_book_ids,bounds,date,industryConstraints,industryNeutral,styleConstraints,styleNeutral):
+    # 检验行业/风格的 自定义约束 和 偏离约束不能存在重复定义
     def _constraintsCheck(constraints, neutral):
         if constraints is None or neutral is None:
             pass
@@ -208,19 +250,33 @@ def validateConstraints(order_book_ids,bounds,date,industryConstraints,industryN
 
     def _boundsCheck(bounds):
         bounds = {} if bounds is None else bounds
-        bounds = bounds.values()
-        lowerCumsum = np.sum(s[0] for s in bounds)
-        upperCumsum = np.sum(s[1] for s in bounds)
+        bounds = bounds
+        lowerCumsum = np.sum(s[0] for s in bounds.values())
 
-        cons1 = (False in [s[0]<=s[1] and s[0]>=0 for s in bounds])
+        for _key in bounds:
+            lower, upper = bounds.get(_key)
+            if lower < 0 or lower > 1:
+                raise InvalidArgument(u'OPTIMIZER: 约束 {} 的下限 {} 无效'.format(_key, lower))
+            if upper < 0 or upper > 1:
+                raise InvalidArgument(u'OPTIMIZER: 约束 {} 的上限 {} 无效'.format(_key, upper))
+            if lower > upper:
+                raise InvalidArgument(u'OPTIMIZER: 约束的下限 {} 高于上限 {}'.format(lower, upper))
 
-        # 假设下限之和>1 或者 上限之和<1 或者某资产的上界小于下界，某一资产的下界小于0
-        if len(bounds) > 0 and (lowerCumsum>1 or cons1 ):
-            raise Exception("请确认个股或行业上下界的合理性")
+        if len(bounds) > 0 and lowerCumsum>1 :
+            raise Exception("OPTIMIZER: 约束的下限之和大于1")
+    #     对于行业
     _boundsCheck(bounds)
     _boundsCheck(industryConstraints)
+
+    upperCumsum = np.sum(s[1] for s in bounds.values())
+    #
+    if upperCumsum<1:
+        raise InvalidArgument(u'OPTIMIZER: 约束的上限之和小于1')
+
     _constraintsCheck(industryConstraints,industryNeutral)
     _constraintsCheck(styleConstraints,styleNeutral)
+    if sorted(set(shenwan_instrument_industry(order_book_ids)['index_name'])) == sorted(industryConstraints.keys()) and sum([s[1] for s in industryConstraints.values()])<1:
+        raise Exception("order_book_ids 权重之和小于 1, 请重新定义行业权重上下限")
 
     missing_industry = set(industryConstraints)- set(missing_industryLabel_handler(order_book_ids,date))
     if missing_industry:
@@ -304,7 +360,7 @@ def style_neutralize_constraint(order_book_ids,date,deviation,factors,benchmark)
     :return:
     """
 
-    constrainedStyle = ['beta', 'momentum', 'size', 'earnings_yield', 'residual_volatility', 'growth',
+    constraintedStyle = ['beta', 'momentum', 'size', 'earnings_yield', 'residual_volatility', 'growth',
                         'book_to_price',
                         'leverage', 'liquidity', 'non_linear_size']
 
@@ -320,9 +376,9 @@ def style_neutralize_constraint(order_book_ids,date,deviation,factors,benchmark)
     constraints = []
 
     if "*"== factors:
-        constrainted_style = constrainedStyle
+        constrainted_style = constraintedStyle
     elif isinstance(factors,list):
-        constrainted_style = sorted(set(constrainedStyle)&set(factors))
+        constrainted_style = sorted(set(constraintedStyle)&set(factors))
     else:
         raise Exception("请在风格偏离约束中指定 * 或者风格因子列表")
 
@@ -360,68 +416,6 @@ def missing_industryLabel_handler(order_book_ids,date):
 
 # 2018-07-09 calculating var and cvar
 
-def calc_var(x,**kwargs):
-    covariance = kwargs.get("covariance")
-    specific = kwargs.get("specific")
-    factor_exposure = kwargs.get("factor_exposure")
-
-    confidence_level = 5
-
-    vol = (x.T.dot(((factor_exposure.dot(covariance)).dot(factor_exposure.T)+specific))).dot(x)
-    var = norm.interval(1-confidence_level/100,0,vol)[0]
-    return var,vol
-
-
-def VaR(x,**kwargs):
-    return -calc_var(x,**kwargs)[0]
-
-
-def CVaR(x,**kwargs):
-    var,vol = calc_var(x,**kwargs)
-    return np.sum([0.001* norm(0,vol).pdf(i) for i in np.arange(-4*vol,var,0.001)])
-
-
-def mean_variance(x,**kwargs):
-
-    annualized_return = kwargs.get("annualized_return")
-    portfolio_volatility = volatility(x,**kwargs)
-
-    return -x.dot(annualized_return) + portfolio_volatility
-
-def maximizing_return(x,**kwargs):
-    annualized_return = kwargs.get("annualized_return")
-    return -x.dot(annualized_return)
-
-
-def maximizing_indicator(x,**kwargs):
-    indicator_series = kwargs.get("indicator_series")
-    return -x.dot(indicator_series)
-
-def risk_budgeting(x,**kwargs):
-    riskMetrics = kwargs.get("riskMetrics")
-    if riskMetrics == "volatility":
-        return volatility(x, **kwargs)
-    else:
-        assert riskMetrics == "tracking_error"
-        return trackingError(x,**kwargs)
-
-def risk_parity(x,**kwargs):
-
-    c_m = kwargs.get("covMat")
-
-    def risk_parity_with_con_obj_fun(x):
-        temp1 = np.multiply(x, np.dot(c_m, x))
-        temp2 = temp1[:, None]
-        return np.sum(scsp.distance.pdist(temp2, "euclidean"))
-
-    if kwargs.get("with_cons"):
-        return risk_parity_with_con_obj_fun(x,c_m=c_m)
-    else:
-        c=15
-        res =  np.dot(x, np.dot(c_m, x)) - c * sum(np.log(x))
-        # print(res)
-        return res
-
 
 def fund_type_constraints(order_book_ids,fundTypeConstraints):
     """
@@ -444,7 +438,114 @@ def fund_type_constraints(order_book_ids,fundTypeConstraints):
 
 
 
+def calcTransactionCost(order_book_ids,x,date,assetType,transactionOptions):
 
+    transactionOptions = {s[0]: s[1] for s in transactionOptions.items() if s[1] is not None}
+
+    initialCapital = transactionOptions.get("initialCapital")
+    currentPositions = transactionOptions.get("currentPositions")
+    holdingPeriod = transactionOptions.get("holdingPeriod",21)
+    commission = transactionOptions.get("commission",True)
+    subscriptionRedemption = transactionOptions.get("subscriptionRedemption")
+    stampDuty = transactionOptions.get("stampDuty")
+    marketImpact = transactionOptions.get("marketImpact",True)
+    commissionRatio = transactionOptions.get("commissionRatio",0.0008)
+    subRedRatio = transactionOptions.get("subRedRatio",{})
+    marketImpactRatio = transactionOptions.get("marketImpactRatio",1)
+    customizedCost = transactionOptions.get("customizedCost")
+    cashPosition = transactionOptions.get("cashPosition",0)
+    output = transactionOptions.get("output")
+
+    commissionRatio = commissionRatio if commission else 0
+    marketImpactRatio = marketImpactRatio if marketImpact else 0
+
+    defaultSubRedRatio = {"Stock": (0.015, 0.005), "Bond": (0.006, 0.005),
+                   "Hybrid": (0.015, 0.005), "StockIndex": (0.015, 0.005),
+                   "BondIndex": (0.006, 0.005), "Related": (0.006, 0.005),
+                   "QDII": (0.016, 0.005), "ShortBond": (0, 0),
+                   "Money": (0, 0), "Other": (0, 0)}
+
+    subRedRatio = {} if subRedRatio is None else subRedRatio
+    defaultSubRedRatio.update(subRedRatio)
+    subRedRatio = defaultSubRedRatio if subscriptionRedemption else {}
+
+    # 获得当前持仓的最新价格
+    cash = currentPositions['cash'] if currentPositions is not None and "cash" in currentPositions.index else 0
+    holding_assets = currentPositions[~(currentPositions.index == "cash")].index.tolist() if currentPositions is not None else []
+    all_assets = sorted(set(holding_assets) | set(order_book_ids))
+    latest_price = fund.get_nav(all_assets,start_date=date,end_date=date,fields="adjusted_net_value").iloc[0] if assetType == "Fund" else get_price(all_assets,start_date=date,end_date=date,fields="close",adjust_type="pre").iloc[0]
+
+    # 获得当前持有的权益
+    market_value = (latest_price*currentPositions[~("cash" == currentPositions.index)]).replace(np.nan,0) if currentPositions is not None else latest_price*0
+    #  总权益
+    total_equity = initialCapital if currentPositions is None else market_value.sum()+cash
+    # 用户非现金权益
+
+    allocated_equity = total_equity*(1-cashPosition)
+    # 获得权益的变化
+    x = pd.Series(x,index=order_book_ids).reindex(all_assets).replace(np.nan,0)
+    x/=x.sum()
+
+    position_delta = (x*allocated_equity) - market_value
+    long_position = position_delta[position_delta>0]
+    short_position = -position_delta[position_delta<0]
+
+    customizedCost = pd.Series(0,index=all_assets) if not isinstance(customizedCost,pd.Series) else customizedCost
+
+    # 对于基金
+    if assetType == "Fund":
+        item_names = ['申赎费用', '申赎费率', '申赎份额']
+        subscription_ratios = pd.Series({s.order_book_id:subRedRatio.get(s.fund_type,(0,0))[0]for s in fund.instruments(long_position.index.tolist())})
+        redemption_ratios = pd.Series({s.order_book_id:subRedRatio.get(s.fund_type,(0,0))[1] for s in fund.instruments(short_position.index.tolist())})
+
+        # 申购时采用前端收费，外扣法计算方式
+        net_subscription_value = long_position/(1+subscription_ratios)
+        subscription_fees = long_position - net_subscription_value
+        subscription_costs = pd.concat([subscription_fees,subscription_fees/long_position,net_subscription_value/latest_price.loc[long_position.index]],axis=1)
+        subscription_costs.columns = item_names
+        subscription_costs['交易方向'] = "申购"
+
+        #  赎回费用
+        redemption_costs = pd.concat([redemption_ratios*short_position,redemption_ratios,short_position/latest_price.loc[short_position.index]],axis=1)
+        redemption_costs.columns = item_names
+        redemption_costs['交易方向'] = '赎回'
+
+        merged_data = pd.concat([subscription_costs,redemption_costs])
+        merged_data['交易金额'] = pd.concat([long_position, short_position])
+        merged_data['自定义费率'] = customizedCost.loc[all_assets].replace(np.nan,0)
+        merged_data['总费率'] = merged_data['申赎费率']+merged_data['自定义费率']
+        merged_data['自定义费用'] = merged_data['自定义费率']*merged_data['交易金额']
+
+    else:
+        assert assetType == "CS"
+        # 对于股票
+        data = get_price(all_assets,pd.Timestamp(date)-np.timedelta64(400,"D"),date,fields=["close",'volume'],adjust_type="pre").iloc[-253:]
+        close_price = data['close']
+        volume = data['volume']
+        daily_volatility = close_price.pct_change().dropna(how="all").std()
+        avg_volume = volume.iloc[-5:].mean()
+
+        positions = pd.concat([short_position,long_position])
+        positions.name = "交易金额"
+
+        sides = pd.Series(["卖" if s in short_position.index else "买" for s in all_assets],index=all_assets)
+        amounts = positions/latest_price
+
+        clearing_time = amounts / avg_volume
+        impact_cost = marketImpactRatio * daily_volatility * clearing_time**(1/2)
+        stamp_duty_cost = pd.Series([0.001 if stampDuty and s in short_position.index  else 0 for s in all_assets],index=all_assets)
+
+        commission = pd.Series([commissionRatio]*len(all_assets),index=all_assets)
+
+        merged_data = pd.DataFrame({"交易股数":amounts,"交易金额":positions,"印花税费率":stamp_duty_cost,"佣金费率":commission,"市场冲击成本":impact_cost,"出清时间（天）":clearing_time,"交易方向":sides,"自定义费率":customizedCost.loc[all_assets].replace(np.nan,0)})
+        merged_data['总交易费用'] = commission+impact_cost+merged_data['自定义费率']+stamp_duty_cost
+    if output:
+        return merged_data
+    else:
+        if assetType == "CS":
+            return merged_data['总交易费用']*(252/holdingPeriod)
+        else:
+            return merged_data['总费率']*(252/holdingPeriod)
 
 
 
